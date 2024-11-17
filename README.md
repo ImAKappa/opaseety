4d4a8c3ac81dc9aeb321eafb5a9769dac5cbd670
ImAKappa <imaninconsp1cuouskappa@gmail.com>
2024-11-17 00:39:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# opaseety
+
+[![PyPI - Version](https://img.shields.io/pypi/v/opaseety.svg)](https://pypi.org/project/opaseety)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/opaseety.svg)](https://pypi.org/project/opaseety)
+
+An image guessing game.
+
+-----
+
+# How to play
+
+```bash
+python opaseety.py 
+```
+
+# Contributing
+
+Requires [hatch](https://hatch.pypa.io/latest/), a modern Python project manager.
+
+```
+git clone 
+```
+
+```
+cd opaseety
+```
+
+```
+hatch shell
+```
+
+Run example with 
+
+```
+python .\opaseety\opaseety.py -i .\data\
+```
+
+
+## Table of Contents
+
+- [opaseety](#opaseety)
+- [How to play](#how-to-play)
+- [Contributing](#contributing)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [License](#license)
+
+## Installation
+
+```console
+pip install opaseety
+```
+
+## License
+
+`opaseety` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ opaseety/opaseety.py
@@ -0,0 +1,53 @@
+"""blend2.py
+
+Lorem Picsum: https://picsum.photos/
+    Request specific size: https://picsum.photos/600
+    Request specific image: https://picsum.photos/id/21/600 
+Blending images: https://www.codespeedy.com/python-pil-image-blend-method/
+"""
+
+import argparse
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from pathlib import Path
+
+def blend(list_images): # Blend images equally.
+
+    equal_fraction = 1.0 / (len(list_images))
+
+    output = np.zeros_like(list_images[0])
+
+    for img in list_images:
+        output = output + img * equal_fraction
+
+    output = output.astype(np.uint8)
+    return output
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Opaseety")
+    parser.add_argument("--imagesets", "-i", required=True, type=Path, help="Directory containing sets of images to blend")
+    # TODO:
+    # parser.add_argument("--resize", "r", type=int, help="Dimension to resize")
+    
+    args = parser.parse_args()
+
+    dim = (600, 600)
+
+    imagesets_dir: Path = args.imagesets
+
+    for img_dir in imagesets_dir.iterdir():
+        print(f"Blending {img_dir.name}")
+        # Convert each image to Image object
+        images = [mpimg.imread(img_path) for img_path in img_dir.iterdir()]
+        print("\tCollected images")
+        resized_images = [cv2.resize(img, dim) for img in images]
+        print("\tResized")
+        # Blend images
+        final_img = blend(images)
+        final_name = imagesets_dir/f"./{img_dir.name}-final-blend.jpg"
+        print(f"\tCompleted blending for {final_name}")
+        plt.imsave(final_name, final_img)
+        print("\tSaved")
